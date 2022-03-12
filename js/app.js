f5d218d4b616fce671381e2f5dd43cc5051612a9
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-03-12 16:40:03 +0330

    Use my github avetar for my profile pic

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 "use strict";
 
+// const { data } = require("autoprefixer");
+
 // Elements
 const root = document.documentElement;
 const togglerBtn = document.querySelector(".mode-toggler");
+const myAvetar = document.querySelector("#profile-pic");
 const githubImgs = document.querySelectorAll(".img");
 const reposContainer = document.querySelector("#repos-container");
 const reposContainerTitle = document.querySelector(".repos-title");
@@ -11,7 +14,24 @@ const loadingSVG = document.querySelector(".loading-svg");
 
 // Initial variables
 const myGithubUsername = "AhmadrezaMozaffary";
+const avetarAPI = `https://api.github.com/users/${myGithubUsername}`;
 const allReposAPI = `https://api.github.com/users/${myGithubUsername}/repos`;
+
+// Recive and display my github profile pic from API
+const setMyAvetar = async function () {
+  try {
+    const responseAPI = await fetch(avetarAPI);
+    console.log(responseAPI);
+    if (!responseAPI.ok)
+      throw new Error(`Avetar is NOT loaded (${response.status})`);
+
+    const { avatar_url: avetarUrl } = await responseAPI.json();
+    avetarUrl && myAvetar.setAttribute("src", `${avetarUrl}`);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+setMyAvetar();
 
 // Recive and display data from API
 const reciveData = async function () {
@@ -21,8 +41,7 @@ const reciveData = async function () {
       throw new Error(`Check your connection ${response.status}`);
 
     const dataArr = await responseAPI.json();
-
-    reposContainerTitle.textContent = `Github Repos (${dataArr.length})`;
+    reposContainerTitle.textContent = `Top github Repos (${dataArr.length})`;
     dataArr.forEach((data, i) => {
       const repoName = data.name.split("-").join(" "); // Remove dashes
       let repoDesc = data.description;
